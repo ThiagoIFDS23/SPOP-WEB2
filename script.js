90e68c7c8625c9e290099ccd381952ef0876401c
ThiagoIFDS23 <129286938+ThiagoIFDS23@users.noreply.github.com>
2024-08-15 10:04:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//Item 1
+console.log("Script Carregando");
+
+//Item 2
+function calcularAnos();
+
+//Item 3
+console.log("Botão de Calcular clicando");
+
+//Item 4
+let popAInput = document.getElementById("popA").value.trim();
+
+//Item 5
+let taxaAInput = document.getElementById("taxaA").value.trim();
+
+//Item 6
+let popBInput = document.getElementById("popB").value.trim();
+
+//Item 7
+let taxaBInput = document.getElementById("taxaB").value.trim();
